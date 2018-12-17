460ad702f93a5edbd9191c2877bdf9d0f07efb0c
pvictor <perrier.victor@gmail.com>
2018-12-17 17:21:17 +0100

    example

--- R/busy-spinner.R
+++ R/busy-spinner.R
@@ -1,7 +1,7 @@
 
 #' @title Busy indicator (spinner)
 #'
-#' @description Add a spinner in an application to show the user that 'Shiny' is busy.
+#' @description Add a spinner in an application each time the server take more 100 milliseconds to respond.
 #'
 #' @param spin Style of the spinner, choice between : \code{circle}, \code{bounce}, \code{folding-cube},
 #'  \code{rotating-plane}, \code{cube-grid}, \code{fading-circle}, \code{double-bounce}, \code{dots}, \code{cube}.
@@ -21,8 +21,59 @@
 #'
 #' @examples
 #' if (interactive()) {
+#'   library(shiny)
+#'   library(shinybusy)
 #'
+#'   ui <- fluidPage(
 #'
+#'     # Use this function somewhere in UI
+#'     add_busy_spinner(spin = "fading-circle"),
+#'
+#'     headerPanel('Iris k-means clustering'),
+#'
+#'     sidebarLayout(
+#'       sidebarPanel(
+#'         selectInput('xcol', 'X Variable', names(iris)),
+#'         selectInput('ycol', 'Y Variable', names(iris),
+#'                     selected=names(iris)[[2]]),
+#'         numericInput('clusters', 'Cluster count', 3,
+#'                      min = 1, max = 9),
+#'         actionButton("sleep", "Long calculation")
+#'       ),
+#'       mainPanel(
+#'         plotOutput('plot1')
+#'       )
+#'     )
+#'   )
+#'
+#'   server <- function(input, output, session) {
+#'
+#'     selectedData <- reactive({
+#'       iris[, c(input$xcol, input$ycol)]
+#'     })
+#'
+#'     clusters <- reactive({
+#'       kmeans(selectedData(), input$clusters)
+#'     })
+#'
+#'     output$plot1 <- renderPlot({
+#'       palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+#'                 "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+#'
+#'       par(mar = c(5.1, 4.1, 0, 1))
+#'       plot(selectedData(),
+#'            col = clusters()$cluster,
+#'            pch = 20, cex = 3)
+#'       points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+#'     })
+#'
+#'     observeEvent(input$sleep, {
+#'       Sys.sleep(5)
+#'     })
+#'
+#'   }
+#'
+#'   shinyApp(ui, server)
 #' }
 add_busy_spinner <- function(spin = "double-bounce", color = "#112446", timeout = 100,
                              position = c("top-right", "top-left", "bottom-right", "bottom-left", "full-page"),
